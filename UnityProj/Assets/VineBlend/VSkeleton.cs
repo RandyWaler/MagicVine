54eb8ea3b1c8ecb6fcd769691706ccbb9e75b621
RandyWaler <867924722@qq.com>
2022-06-19 18:38:15 +0800

    skeleton Drop Touch Ground Bug Fix

--- UnityProj/Assets/VineBlend/VSkeleton.cs
+++ UnityProj/Assets/VineBlend/VSkeleton.cs
@@ -12,11 +12,14 @@ public class VSkeleton : VBox1
 
     private float nowStandTime = -1;
 
+    private int idleHash = Animator.StringToHash("Idle");
     private int takeHash = Animator.StringToHash("Take");
     private int fallsHash = Animator.StringToHash("FallS");
     private int falleHash = Animator.StringToHash("FallE");
     private int standHash = Animator.StringToHash("Stand");
     private int fallingHash = Animator.StringToHash("Falling");
+
+    bool falleFlag = false;
 
     protected override void Awake() {
         base.Awake();
@@ -31,14 +34,18 @@ public class VSkeleton : VBox1
 
     public override void onDrop() {
         animator.SetTrigger(fallsHash);
+        falleFlag = true;
         base.onDrop();
     }
 
 
-    private void OnCollisionEnter(Collision collision) {
-        if (animator.GetCurrentAnimatorStateInfo(0).shortNameHash == fallingHash) {
+    //这里骷髅兵的释放点没有高度限制，可能贴地释放，因此要用OnCollisionStay持续检测+boolflag保证Trigger只Set一次
+    private void OnCollisionStay (Collision collision) {
+        if (falleFlag&&animator.GetCurrentAnimatorStateInfo(0).shortNameHash == fallingHash) {
             animator.SetTrigger(falleHash);
             nowStandTime = 0f;
+            falleFlag = false;
+
         }
     }
 
